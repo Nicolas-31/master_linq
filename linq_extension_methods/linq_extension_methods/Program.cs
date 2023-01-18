61bb62ee93d3b631ca7cfe711d3046e7fd0b151c
Nicolas Anderson <nicolas-31@outlook.com>
2023-01-18 18:23:35 +0100

    updated

--- linq_extension_methods/linq_extension_methods/Program.cs
+++ linq_extension_methods/linq_extension_methods/Program.cs
@@ -17,12 +17,46 @@ namespace LinqTutorial
     {
         static void Main(string[] args)
         {
-            var words = new[] { "a", "bb", "ccc", "dddd" };
-            var wordsLongerThan2Lettters = words.Where(
-                word => word.Length > 2
-            );
-            Console.WriteLine(string.Join(", ", wordsLongerThan2Lettters));
+            var words = new List<string>() { "a", "bb", "ccc", "dddd" };
+            // var wordsLongerThan2Lettters = words.Where(
+            //     word => word.Length > 2
+            // );
+            // Console.WriteLine(string.Join(", ", wordsLongerThan2Lettters));
+            
+            var shortWords = words.Where(word => word.Length < 3).ToList();
+            Console.WriteLine("First iteration");
+            foreach (var word in shortWords)
+            {
+                // deferred execution
+                Console.WriteLine(word);
+            }
+            words.Add("e");
+            Console.WriteLine("Second iteration");
+            foreach (var word in shortWords)
+            {
+                // deferred execution
+                Console.WriteLine(word);
+            }
 
+          
+            // var allAmericans = people.Where(person => person.Country == "USA");
+            // var notAllAmericans = allAmericans.Take(100);
+
+            var animals = new List<string>() { "Duck", "Lion", "Dolphin", "Tiger"};
+            var animalsWithD = animals.Where(animal =>
+            {
+                Console.WriteLine("checking animals: " + animal);    
+                return animal.StartsWith("D");
+            });
+            foreach (var animal  in animalsWithD)
+            {
+                Console.WriteLine(animal);
+            }
+            
+            // here duck and dolphin will be printed twice at the same time
+            // because the query is executed in the same time as the foreach loop iterates
+            
+            
             var multilineString = @"Please give me a ride on your back,
                                   Said the Duck to the Kangaroo:
                                   I would sit quite still, and say nothing but 'Quack'
@@ -43,6 +77,24 @@ namespace LinqTutorial
                 .Where(number => number % 2 == 1)
                 .OrderBy(number => number);
             Console.WriteLine("orderedOddNumbers: " + string.Join(",", orderedOddNumbers));
+            
+            
+            // method syntax vs query syntax
+            var numbers2 = new List<int> { 4,2,7,10,12,5,4,2 };  
+            var smallOrderedNumbersMethodSyntax = 
+                numbers2
+                    .Where(number => number <10)
+                     .OrderBy(number => number)
+                    .Distinct();
+            Console.WriteLine(string.Join(",", smallOrderedNumbersMethodSyntax));
+
+            var smallOrderedNumbersQuerySyntax =
+                (from number in numbers2
+                where number < 10
+                orderby number
+                select number).Distinct();
+
+            Console.WriteLine(string.Join(",", smallOrderedNumbersQuerySyntax));
             Console.ReadKey();
         }
     }
